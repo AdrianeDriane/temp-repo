91cfe1c2fb40d119c5e506870c80103942192637
Adriane Dilao <dilaoaparre@gmail.com>
2023-07-22 17:30:17 +0800

    first commit

---
+++ 4-names.js
@@ -0,0 +1,9 @@
+// local
+const secret = 'Secret Message';
+
+// share
+const zydric = 'zydric';
+const adriane = 'adriane';
+const vince = 'vince';
+
+module.exports = { zydric, adriane, vince };
---
+++ 6-alternative-flavor.js
@@ -0,0 +1,5 @@
+module.exports.items = ['ice-cream', 'fried chicken'];
+module.exports.person = {
+  name: 'Bob',
+  age: 18,
+};
